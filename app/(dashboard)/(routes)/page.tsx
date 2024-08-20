515bfd2f4a7ef600afc3a150339e8db825f40ae5
Mahesh <maheshsp1809@gmail.com>
2024-08-20 20:45:47 +0530

    unsigned routes

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+
+// he worked with next 13.4.12
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-semibold text-sky-700">This is a protected page</p>
+    </div>
+  )
+}
