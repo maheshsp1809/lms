a4b0f120e45068e950e3c6493fbbaa2f34ebd369
Mahesh <maheshsp1809@gmail.com>
2024-08-20 19:48:22 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-semibold text-sky-700">Mahesh</p>
+      <Button >Clcik me </Button>
+    </div>
+
+  )
+}
